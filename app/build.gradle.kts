23a183dfcf3f9adb3c7f63aa72c516ab6965ae4f
znmyth <znmyth@gmail.com>
2025-05-24 02:16:40 +0330

    finished SigninActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,6 +12,7 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
+        multiDexEnabled = true
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -40,4 +41,16 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+
+    implementation("com.mohamadamin:persianmaterialdatetimepicker:1.2.1")
+    implementation("com.intuit.sdp:sdp-android:1.1.1")
+    implementation("com.intuit.ssp:ssp-android:1.1.1")
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("com.android.support:multidex:1.0.3")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-scalars:2.11.0")
+
+    implementation("androidx.swiperefreshLayout:swiperefreshlayout:1.1.1")
 }
